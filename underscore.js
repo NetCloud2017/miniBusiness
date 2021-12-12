a17c498153a5dbfdc9ed011c049639dc486068d9
DESKTOP-NCK2EPM\ironMax <tothinktodo@163.com>
2021-12-12 23:10:28 +0800

    madd underscore function module

---
+++ underscore.js
@@ -0,0 +1,14 @@
+var _ = (underscore = (function (root) {
+    var _ = function (obj) {
+        if (obj instanceof _) {
+            return obj
+        }
+        if (! (this instanceof _)) {
+            return new _(obj)
+        }
+    }
+    _.uniq = _unique = function(array, isSorted, iteratee, context) {
+        
+    }
+    root.underscore = root._ = _;
+})(this));
