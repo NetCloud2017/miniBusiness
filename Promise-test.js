ff4aeb7f22269d08f784e98938503e7d4a039907
DESKTOP-NCK2EPM\ironMax <tothinktodo@163.com>
2021-12-25 23:16:25 +0800

    feat: promise test

--- Promise-test.js
+++ Promise-test.js
@@ -1,3 +1,22 @@
+new Promise((resolve, reject) => {
+    console.log('初始化');
+
+    resolve();
+})
+.then(() => {
+    throw new Error('有哪里不对了');
+
+    console.log('执行「这个」”');
+})
+.catch(() => {
+    console.log('执行「那个」');
+})
+.then(() => {
+    console.log('执行「这个」，无论前面发生了什么');
+});
+
+
+// 
 function fn(val) {
     console.log(val);
 }
