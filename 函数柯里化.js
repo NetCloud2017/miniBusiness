89a3800563574c02d9dcbc24b6fa6bd7e06b1b77
DESKTOP-NCK2EPM\ironMax <tothinktodo@163.com>
2022-07-19 16:51:22 +0800

    函數柯里化 方法2

--- 函数柯里化.js
+++ 函数柯里化.js
@@ -33,19 +33,28 @@ true
 // 弊端是需要依赖 fn 的length 属性 ， 不够灵活。
 function progressCurrying(fn, args) {
     var _this = this;
-    var len = fn.length;
-    var args = args || [];
-
+    var len = fn.length; // length 是fn 的形参个数
+    console.log(len, "len");
+    var args = [].slice.call(arguments, 1);
     return function () {
         var _args = Array.prototype.slice.call(arguments);
-        Array.prototype.push.apply(args, _args);
 
+        args.push(..._args);
+        console.log(args,'22');
         // 如果参数个数小于最初的fn.length，则递归调用，继续收集参数
-        if (_args.length < len) {
-            return progressCurrying.call(_this, fn, _args);
+        if (args.length < len) {
+            return progressCurrying.call(_this, fn, ..._args);
         }
 
         // 参数收集完毕，则执行fn
-        return fn.apply(this, _args);
+        return fn.apply(this, args);
     };
 }
+
+function multiply(a, b) {
+    return a * b;
+}
+
+let aa = progressCurrying(multiply)(2, 3)
+
+console.log(aa, "aa");
